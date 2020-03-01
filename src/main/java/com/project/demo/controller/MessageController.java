64349e5ad62fb1e3cafe5fef06238d7f6fe81185
Roman <roma-govor2013@ya.ru>
2020-03-01 22:29:30 +0300

    Open this repository in IntelliJ IDEA and compile. Then through localhost:8080 can open this project

---
+++ src/main/java/com/project/demo/controller/MessageController.java
@@ -0,0 +1,70 @@
+package com.project.demo.controller;
+
+import com.project.demo.exeptions.NotFoundExceptions;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("testmessage")                    //Запрос
+public class MessageController{
+    private int counter = 4;
+
+    private List <Map<String,String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap <String,String>() {{
+                put("id", "1");
+                put("Text", "Message 1"); }
+        });
+        add(new HashMap <String,String>() {{
+            put("id", "2");
+            put("Text", "Message 2"); }
+        });
+        add(new HashMap <String,String>() {{
+            put("id", "3");
+            put("Text", "Message 3"); }
+        });
+    }};
+    @GetMapping
+    public List <Map<String,String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id){
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(messages -> messages.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundExceptions::new);
+    }
+
+    @PostMapping              //Добавление нового метода
+    public Map<String,String> create(@RequestBody Map<String, String> message) {
+        message.put("id",String.valueOf(counter++));
+        messages.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")                                                          //Добавление текущей записи
+    public   Map<String,String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id",id);
+
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String,String> message = getMessage(id);
+
+        messages.remove(message);
+    }
+}
---
+++ src/main/java/com/project/demo/exeptions/NotFoundExceptions.java
@@ -0,0 +1,8 @@
+package com.project.demo.exeptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundExceptions extends RuntimeException{
+}
